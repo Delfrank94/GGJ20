30928f6dda68f18596f7cb230be02989bc6eead0
Delfrank94 <Delfrank94@gmail.com>
2020-01-31 21:08:08 -0300

    Added movable piece

--- Piece.gd
+++ Piece.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
+
+export var moveForce = 50
 func _physics_process(_delta):
 	if Input.is_action_pressed("ui_left"):
-		apply_impulse(Vector2.ZERO,Vector2.LEFT)
+		apply_impulse(Vector2.ZERO,Vector2.LEFT * moveForce)
 	if Input.is_action_pressed("ui_right"):
-		apply_impulse(Vector2.ZERO,Vector2.RIGHT)
+		apply_impulse(Vector2.ZERO,Vector2.RIGHT * moveForce)
 	if Input.is_action_pressed("ui_down"):
-		apply_impulse(Vector2.ZERO,Vector2.DOWN)
+		apply_impulse(Vector2.ZERO,Vector2.DOWN * moveForce)
