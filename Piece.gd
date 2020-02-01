0dea4da687fc205a2b9f2867d602a1d37dcc38bf
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2020-02-01 13:47:05 -0300

    SFX: add rush fx to pieces

--- Piece.gd
+++ Piece.gd
@@ -19,6 +19,8 @@ func _physics_process(_delta):
 			apply_impulse(Vector2.ZERO,Vector2.RIGHT * moveForce)
 		if Input.is_action_pressed("ui_down"):
 			apply_impulse(Vector2.ZERO,Vector2.DOWN * moveForce)
+		if Input.is_action_just_pressed("ui_down"):
+			SFX.play_fx(SFX.FX.RUSH)
 
 func landed():
 	active = false
--- SFX.gd
+++ SFX.gd
@@ -1,16 +1,19 @@
 extends Control
 
 const MAX_AUDIO_PLAYERS = 8
+var ONE_AT_TIME : Array = [ FX.RUSH ]
 
 var _audio_players : Array
+var _stream_tags : Dictionary
 var _audio_player_idx : int = 0
 
-enum FX { COIN, JUMP, LAND }
+enum FX { COIN, JUMP, LAND, RUSH }
 
 const streams = {
 	FX.COIN: preload("res://sound/coin.wav"),
 	FX.JUMP: preload("res://sound/jump.wav"),
-	FX.LAND: preload("res://sound/land.wav")
+	FX.LAND: preload("res://sound/land.wav"),
+	FX.RUSH: preload("res://sound/rush.wav")
 }
 
 func _ready():
@@ -21,8 +24,17 @@ func _ready():
 		add_child(_audio_players[i])
 
 func play_fx(key):
-	if streams.has(key):
+	if streams.has(key) and not(ONE_AT_TIME.has(key) and _is_playing(key)):
 		var stream : AudioStreamSample = streams[key]
+		_stream_tags[_audio_player_idx] = key
 		_audio_players[_audio_player_idx].set_stream(stream)
 		_audio_players[_audio_player_idx].play()
+		_audio_players[_audio_player_idx].connect("finished", self, "_stream_untag", [_audio_player_idx])
 		_audio_player_idx = (_audio_player_idx + 1) % MAX_AUDIO_PLAYERS
+
+func _stream_untag(idx):
+	_stream_tags.erase(idx)
+
+func _is_playing(key):
+	return _stream_tags.values().has(key)
+	
