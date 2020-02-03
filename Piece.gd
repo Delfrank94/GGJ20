98e8d26e08b6e4ef3e1ffa3c7d1a6dde246a2e6c
Delfrank94 <Delfrank94@gmail.com>
2020-02-03 16:09:24 -0300

    added delta time in calcaulation

--- Piece.gd
+++ Piece.gd
@@ -3,23 +3,23 @@ extends RigidBody2D
 class_name Piece
 var active = true
 signal landed
-var moveForce = 30
+var moveForce = 1800
 export var hazard = false
 
 func _ready():
 	rotation_degrees = 45 * (randi() % 5)
-	apply_impulse(Vector2.ZERO,Vector2.DOWN * moveForce)
+	apply_impulse(Vector2.ZERO,Vector2.DOWN * moveForce * get_process_delta_time() )
 
 func _physics_process(_delta):
 	if active:
 		if Input.is_action_just_pressed("ui_up"):
 			global_rotation_degrees += 45
 		if Input.is_action_pressed("ui_left"):
-			apply_impulse(Vector2.ZERO,Vector2.LEFT * moveForce)
+			apply_impulse(Vector2.ZERO,Vector2.LEFT * moveForce * _delta)
 		if Input.is_action_pressed("ui_right"):
-			apply_impulse(Vector2.ZERO,Vector2.RIGHT * moveForce)
+			apply_impulse(Vector2.ZERO,Vector2.RIGHT * moveForce * _delta)
 		if Input.is_action_pressed("ui_down"):
-			apply_impulse(Vector2.ZERO,Vector2.DOWN * moveForce)
+			apply_impulse(Vector2.ZERO,Vector2.DOWN * moveForce * _delta)
 		if position.y >= 1080:
 			landed()
 			emit_signal("landed", self)
